ca26a7fc111972a4f06b179740f41644300aa1ce
Marc <Marc@DESKTOP-C5OU0Q8>
2017-03-27 20:08:17 +0200

    Agregar archivos de proyecto.

---
+++ WebIncidencias UF3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebIncidencias_UF3.MvcApplication" Language="C#" %>
---
+++ WebIncidencias UF3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebIncidencias_UF3.Startup))]
+namespace WebIncidencias_UF3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
